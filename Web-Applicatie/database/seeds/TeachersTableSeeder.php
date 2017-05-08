7cbe65df613c3b23c36fcf799ed4b8f88c8fc26f
JelleS-immalle <jelle.swaelen@immalle.be>
2017-05-08 19:22:49 +0200

    Seeder toegevoegd voor Teachers

--- Web-Applicatie/database/seeds/TeachersTableSeeder.php
+++ Web-Applicatie/database/seeds/TeachersTableSeeder.php
@@ -11,5 +11,39 @@ class TeachersTableSeeder extends Seeder
      */
     public function run()
     {
+    	DB::table('teachers')->insert([
+            'id' => 1, 'teacherName' => 'Van Broeckhoven H.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 2, 'teacherName' => 'Cauwenberg B.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 3, 'teacherName' => 'Philips J.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 4, 'teacherName' => 'De Schutter G.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 5, 'teacherName' => 'Bens M.-L.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 6, 'teacherName' => 'Van Dooren M.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 7, 'teacherName' => 'Schryvers S.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 8, 'teacherName' => 'Swaans L.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 9, 'teacherName' => 'van Bergen M.',
+        ]);
+
+        DB::table('teachers')->insert([
+        	'id' => 10, 'teacherName' => 'Vervoort K.',
+        ]);
+        DB::table('teachers')->insert([
+        	'id' => 11, 'teacherName' => 'Degrauwe W.',
+        ]);
     }
 }
