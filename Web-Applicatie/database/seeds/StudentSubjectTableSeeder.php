318355f600da07b406fad2ae7423612eee05892c
JelleS-immalle <jelle.swaelen@immalle.be>
2017-05-08 19:26:38 +0200

    Seeder toegevoegd voor StudentSubject

--- Web-Applicatie/database/seeds/StudentSubjectTableSeeder.php
+++ Web-Applicatie/database/seeds/StudentSubjectTableSeeder.php
@@ -11,6 +11,23 @@ class StudentSubjectTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('student_subject')->insert([
+        	'id' => 1, 'student_id' => 1, 'subject_id'=> 1,
+        ]);
+        DB::table('student_subject')->insert([
+        	'id' => 2, 'student_id' => 2, 'subject_id'=> 1,
+        ]);
+        DB::table('student_subject')->insert([
+        	'id' => 3, 'student_id' => 1, 'subject_id'=> 2,
+        ]);
+        DB::table('student_subject')->insert([
+        	'id' => 4, 'student_id' => 1, 'subject_id'=> 3,
+        ]);
+        DB::table('student_subject')->insert([
+        	'id' => 5, 'student_id' => 2, 'subject_id'=> 3,
+        ]);
+        DB::table('student_subject')->insert([
+        	'id' => 6, 'student_id' => 2, 'subject_id'=> 4,
+        ]);
     }
 }
