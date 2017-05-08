73e4426b43115cd808f15bce90cb88ada83acb21
JelleS-immalle <jelle.swaelen@immalle.be>
2017-05-08 19:16:43 +0200

    Seeder toegevoegd voor Students

--- Web-Applicatie/database/seeds/StudentsTableSeeder.php
+++ Web-Applicatie/database/seeds/StudentsTableSeeder.php
@@ -11,6 +11,11 @@ class StudentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+    	DB::table('students')->insert([
+            'id' => 1, 'studentName' => 'Jelle', 'studentNumber' => 11, 
+        ]);
+        DB::table('students')->insert([
+        	'id' => 2, 'studentName' => 'Jef', 'studentNumber' => 7,
+        ]);
     }
 }
